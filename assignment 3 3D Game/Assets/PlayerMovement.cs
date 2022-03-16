51ea0e473899d9bc51cb7ad45ae209ceb30131a1
Jiachen Tang <tjc9606ok@gmail.com>
2022-03-16 07:02:47 -0400

    player animations for walk and idle added

--- assignment 3 3D Game/Assets/PlayerMovement.cs
+++ assignment 3 3D Game/Assets/PlayerMovement.cs
@@ -9,10 +9,12 @@ public class PlayerMovement : MonoBehaviour
     public float timeOfTurn = 0.1f;
     float speedOfTurn;
 
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +31,87 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             controller.Move(direction * speedOfMove * Time.deltaTime);
+        }
+
+        // triggers for animations
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            anim.SetTrigger("idleTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetTrigger("runTrigger");
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetTrigger("idleTrigger");
         }
     }
 }
