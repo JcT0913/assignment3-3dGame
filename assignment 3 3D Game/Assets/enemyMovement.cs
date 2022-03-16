d2d635464c938e98cfc9ac4a7d7adbb607fbe66d
Jiachen Tang <tjc9606ok@gmail.com>
2022-03-16 11:26:21 -0400

    test for enemy script completed

--- assignment 3 3D Game/Assets/enemyMovement.cs
+++ assignment 3 3D Game/Assets/enemyMovement.cs
@@ -7,7 +7,7 @@ public class enemyMovement : MonoBehaviour
 {
     public GameObject[] guidePoints;
     public NavMeshAgent enemy;
-    // public float dist2Target;
+    public float dist2Player = 5f;
     private int currentGuideIndex = 0;
     public GameObject player;
 
@@ -30,13 +30,14 @@ public class enemyMovement : MonoBehaviour
         if (enemy.remainingDistance <= 0.5f)
         {
             currentGuideIndex += 1;
-            if (currentGuideIndex == guidePoints.Length)
+            if (currentGuideIndex >= guidePoints.Length)
             {
                 currentGuideIndex = 0;
             }
             enemy.SetDestination(guidePoints[currentGuideIndex].transform.position);
         }
 
+        // this raycast works for making enemy walk accoording to guide points
         /*
         RaycastHit hit;
         Ray ray = new Ray(transform.position, guidePoints[currentGuideIndex].transform.position - transform.position);
@@ -46,5 +47,15 @@ public class enemyMovement : MonoBehaviour
             enemy.SetDestination(guidePoints[currentGuideIndex].transform.position);
         }
         */
+
+        // this raycast works for making enemy flees from player
+        float dist = Vector3.Distance(transform.position, player.transform.position);
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, player.transform.position - transform.position);
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Player" && dist < dist2Player)
+        {
+            enemy.SetDestination(transform.position + (transform.position - player.transform.position).normalized * 10);
+        }
     }
 }
